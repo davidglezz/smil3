14fd27fbcbd043d2cd7a14cbee2a93e502777f15
David Gonzalez <davidgg666@gmail.com>
2012-11-05 00:06:59 +0100

    user: davidgg666 changed _otros/database.mwb changed _otros/database.mwb.bak changed php/config.php

--- php/config.php
+++ php/config.php
@@ -7,12 +7,14 @@
 	if ($_SERVER['HTTP_HOST'] != 'smil3.tk')
 	{
 		$user->db['host'] = "localhost";
-		$user->db['user'] = "admin";
+		$user->db['user'] = "smil3";
 		$user->db['pass'] = "smil3";
 		$user->db['name'] = "smil3";
 	}
 
 	//Starts the object by triggering the constructor
 	$user->start();
+
+	var_dump($user);
 	
 ?>
