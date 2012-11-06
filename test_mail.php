4a54cd5e23baed947894a5dc33c648c70c79340a
David Gonzalez <davidgg666@gmail.com>
2012-11-06 13:58:46 +0100

    user: davidgg666 branch 'default' changed test_mail.php

--- test_mail.php
+++ test_mail.php
@@ -16,4 +16,53 @@
     $asunto = 'Contacto desde Taller Webmaster';
     mail($para, $asunto, utf8_decode($mensaje), $header);
     echo '&estatus=ok&';
+    
+    
+?>
+
+
+
+<?php
+// Varios destinatarios
+$para  = 'aidan@example.com' . ', '; // atención a la coma
+$para .= 'wez@example.com';
+
+// subject
+$titulo = 'Recordatorio de cumpleaños para Agosto';
+
+// message
+$mensaje = '
+<html>
+<head>
+  <title>Recordatorio de cumpleaños para Agosto</title>
+</head>
+<body>
+  <p>¡Estos son los cumpleaños para Agosto!</p>
+  <table>
+    <tr>
+      <th>Quien</th><th>Día</th><th>Mes</th><th>Año</th>
+    </tr>
+    <tr>
+      <td>Joe</td><td>3</td><td>Agosto</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17</td><td>Agosto</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// Para enviar un correo HTML mail, la cabecera Content-type debe fijarse
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Cabeceras adicionales
+$cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+$cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
+$cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
+$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+
+// Mail it
+mail($para, $titulo, $mensaje, $cabeceras);
 ?>
