34dad567f622069ea7e6c870b67e72dc979a810c
David Gonzalez <davidgg666@gmail.com>
2013-01-11 00:37:28 +0100

    nueva clase usuario

--- php/class.user2.php
+++ php/class.user2.php
@@ -1,33 +1,95 @@
 <?php
 
 class User extends Singleton
-{
-	var $id;		//Current user ID
-	var $username;	//Signed username
-	var $signed;	//Boolean, true = user is signed-in
+{	
+	public $id;			//Current user ID
+	public $username;	//Signed username
+	public $signed;		//Boolean, true = user is signed-in
 	
-	function login($data)
+	protected function __construct()
 	{
+		$this->signed = false;
+		$this->id = null;
+		$this->username = null;
+	}
+		
+	// se sobreescribe ya que la instancia debe guardarse en la sesion.
+	public static function getInstance()
+	{
+		if (!isset($_SESSION[__CLASS__]))
+			$_SESSION[__CLASS__] = new static;
+
+		return $_SESSION[__CLASS__];
+	}
+
+	public function login($user, $password)
+	{
+		// Validar datos
+		//Validate::string($user, array());
+		
+		// Comprobar datos
 		$db = Database::getInstance();
-		$res = $db->query('select id_user, username, password, salt from users where email = ?', $data);
-		if ($res && $hash === hash('sha256', $salt.$password))
+		$res = $db->query('SELECT id_user, username, password, salt FROM users WHERE username = ? LIMIT 1', array($user));
+		
+		if ($res && $res[2] === hash('sha256', $password.$res[3]))
 		{
-			
-			
+			$this->id = $res[0];
+			$this->username = $res[1];
+			$this->signed = true;
 		}
 	}
 	
-	function register($data)
+	public function logout($data)
 	{
-		// Para evitar que se pueda averiguar la contraseña.
-		$salt = '|'.sha256(uniqid(rand().'Smil3:)', false)); 
-		$password = hash('sha256', $password.$salt);
-		
-		// Guardar en base de datos el $hash y $salt
-		
-		
+		$this->__construct();
 	}
 	
+	public function register($data)
+	{
+		// Validar datos
+		
+		
+		// Generar hash
+		$salt = '|'.hash('sha256', uniqid(rand().'Smil3:)', false), false); 
+		$password = hash('sha256', $password.$salt);
+		
+		// Insertar en la base de datos
+		$db = Database::getInstance();
+		$sql = 'INSERT INTO users (username, password, salt, email, name, birthdate, sex, country, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);';
+		$arg = array($user);
+		$res = $db->query($sql, $arg);
+	}
+	
+	public function update($data)
+	{
+		// Validar datos
+		
+		
+		// Insertar en la base de datos
+		$db = Database::getInstance();
+		$sql = 'INSERT INTO users (username, password, salt, email, name, birthdate, sex, country, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);';
+		$arg = array($user);
+		$res = $db->query($sql, $arg);
+	}
+	
+	public function pass_reset($data)
+	{
+		// Enviar email de confirmación
+		// con un 
+	}
+	
+	public function pass_change($data)
+	{
+
+	}
+	
+	
+	
+	
 }
+
+
+// http://www.php.net/manual/en/function.crypt.php
+// http://stackoverflow.com/questions/4795385/how-do-you-use-bcrypt-for-hashing-passwords-in-php
 
 ?>
--- test.php
+++ test.php
@@ -1,12 +1,43 @@
 <?php
 
-//var_dump(PDO::getAvailableDrivers());
+if (!isset($_SESSION))
+	session_start();
 
-//require_once('php/class.database.php');
-require_once('php/autoload.php');
+class test
+{
+	public $a;
+	public $b;
+	public $c = 'hola';
+	
+	public static function getInstance()
+	{
+		if (!isset($_SESSION[__CLASS__]))
+			$_SESSION[__CLASS__] = new static;
 
-var_dump(Validate::number(50, array('decimal' => false, 'min' => 1, 'max' => 50)));
+		return $_SESSION[__CLASS__];
+	}
+	
+	private function __construct()
+	{
+
+		$this->a = 1;
+		$this->b = 2;
+		$_SESSION['test'] = $this;
+	}
+	
+	public function setB ()
+	{
+		$this->b = 9;
+	}
+}
 
 
+var_dump($_SESSION);
 
+$t = test::getInstance();
+var_dump($_SESSION);
+
+$t->a = 3;
+$t->setB();
+var_dump($_SESSION);
 ?>
