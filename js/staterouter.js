3a43a85db127b0aea5c4c9c4c1782b34b9a42f5c
davidgg666 <davidgg666@localhost>
2013-01-23 00:05:50 +0100

    renombro smil3.htm a index.htm para que sea el documento principal
    pruebas con el ruteo de url

---
+++ js/staterouter.js
@@ -0,0 +1,80 @@
+/*jslint browser: true*/
+/*global History*/
+"use strict";
+
+var staterouter = (function () {
+    function normalizePath(path) {
+        if (path[0] !== '/') {
+            path = '/' + path;
+        }
+        return path;
+    }
+
+    function Router() {
+        var self = this;
+        self.routes = {};
+
+        self.route = function (path, func) {
+            path = normalizePath(path);
+            self.routes[path] = func;
+            return self;
+        };
+
+        self.navigate = function (path, state, title) {
+            path = normalizePath(path);
+            History.pushState(state, title, path);
+            return self;
+        };
+
+        self.perform = function () {
+            var state = History.getState(),
+                // Get pathname part of URL, which is what we'll be matching
+                link = document.createElement("a"),
+                url,
+                route,
+                rx,
+                match,
+                func;
+            link.href = state.url;
+            url = normalizePath(link.pathname);
+            for (route in self.routes) {
+                if (self.routes.hasOwnProperty(route)) {
+                    // Replace :[^/]+ with ([^/]+), f.ex. /persons/:id/resource -> /persons/([^/]+)/resource
+                    rx = new RegExp('^' + route.replace(/:\w+/g, '(\\w+)') + '$');
+                    match = rx.exec(url);
+                    //console.log("Route " + route + ", " + url + ", match: " + match);
+                    if (match !== null) {
+                        // Translate groups to parameters
+                        func = self.routes[route];
+                        //console.log('Route ' + route  + ' matched, arguments: ' + match.slice(1));
+                        func.apply(state, match.slice(1));
+                        break;
+                    }
+                }
+            }
+            return self;
+        };
+
+        self.back = function () {
+            History.back();
+            return self;
+        };
+
+        self.go = function (steps) {
+            History.go(steps);
+            return self;
+        };
+
+        function onStateChange() {
+            //console.log('statechange: ' + History.getState().url);
+            self.perform();
+        }
+        History.Adapter.bind(window, 'statechange', onStateChange);
+    }
+
+    return {
+        Router: Router
+    };
+}());
+
+// vim: set sts=4 sw=4 et:
